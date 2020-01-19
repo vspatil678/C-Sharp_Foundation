5cf0b871c9e32da514a8f5642784c466e78e62c4
Vinoda Patil <vspatil678@gmail.com>
2020-01-19 10:28:05 +0530

    Add project files.

---
+++ PreparationProj/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PreparationProj
+{
+    class Program
+    {
+        // synchronous methods 
+        private static void SyncMethod1()
+        {
+            Console.WriteLine("Executing m1");
+            for(int i=0; i<10; i++)
+            {
+                if(i == 5)
+                {
+                    Task.Delay(5000);
+                }
+                 Console.WriteLine(i);
+            }
+        }
+
+        private static void SyncMethod2()
+        {
+            Console.WriteLine("Executing m2");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+        static void Main1(string[] args)
+        {
+            /* in method 1 we have 5 sec of delay application will be in
+             * sleep mode for 5 sec and method 2 will not execute
+             * once method 1 is completes its execution method 2 execution will start
+             * Note: Single Thread/Task is used to execute both methods
+            */
+           // SyncMethod1();
+           // SyncMethod2();
+            /*
+             * ASyncMethod1() starts execution and application will not sleep for 5 sec immediately 
+             * ASyncMethod2() will start execution application will not block 
+             * Note: Different threads are used
+             */
+           // ASyncMethod1();
+           // ASyncMethod2();
+
+            //Thread t1 = new Thread(ASyncMethod1);
+            //Thread t2 = new Thread(ASyncMethod2);
+            //t1.Start();
+            //t2.Start();
+
+            Task tk1 = new Task(ASyncMethod1);
+            Task tk2 = new Task(ASyncMethod2);
+            tk1.Start();
+            tk2.Start();
+
+            Console.ReadLine();
+        }
+
+        // Asynchronous methods 
+        private static async void ASyncMethod1()
+        {
+            Console.WriteLine("Executing m1 async");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                   await Task.Delay(5000);
+                }
+                Console.WriteLine(i);
+            }
+        }
+
+        private static async void ASyncMethod2()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Executing m2 async");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            });
+        }
+    }
+}
---
+++ PreparationProj/UnderstandStatic.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PreparationProj
+{
+    // public static class UnderstandStatic
+    public static class UnderstandStatic
+    {
+        // constant variables cannot be marked as static
+        // public static const int constVariable = 20;
+
+        static UnderstandStatic()
+        {
+
+        }
+       
+        public static int y;
+       
+        public static int  x = 10;
+        //public int y = 20;
+
+        //public void Method1()
+        //{
+
+        //}
+      
+        public static void Method2()
+        {
+
+        }
+        public static void Method2(int x)
+        {
+
+        }
+    }
+
+    public class NonStaticClass
+    {
+        public NonStaticClass()
+        {
+            Console.WriteLine("ctor in nonstatic class");
+        }
+        static NonStaticClass()
+        {
+            variable1 = 20;
+            Console.WriteLine("static ctor in non-static class "+ variable1);
+        }
+        public static int variable1;
+    }
+
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            UnderstandStatic.x = 20;
+            UnderstandStatic.x = 30;
+            UnderstandStatic.x = 40;
+            Console.WriteLine(UnderstandStatic.x); // 40
+
+            NonStaticClass.variable1 = 20;
+            NonStaticClass.variable1 = 30;
+            NonStaticClass.variable1 = 40;
+            Console.WriteLine(NonStaticClass.variable1); // 40
+
+            NonStaticClass n1 = new NonStaticClass();
+            
+
+            
+        }
+    }
+}
+
+// static class
+/*
+ *  static class should contain only static properties (variables and methods and constructor should be static)
+ *  we cant create object for the static class compiler error
+ *  we canot inherit the static class
+ *  if constructor in the static class that should be static constructor
+ *  static constructor should not have any access specifiers
+ *  static classes are sealed classes
+ *  A static constructor is only called one time,
+ *  and a static class remains in memory for the lifetime of the application 
+ *  domain in which your program resides.
+ *  Static methods can be overloaded but not overridden, because they belong to the class
+ *  Static constroctor must be parameterless
+ *  Static members are initialized before the static member is accessed for the first time
+ *  and before the static constructor, if there is one, is called.
+ *  constant variables cannot be marked as static
+ *  if class have static ctor and non static ctor - static ctor will execute first
+ * */
